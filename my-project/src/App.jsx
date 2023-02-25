ff6c3373248abcfccc7e7254d35d5d0aa80dfd6e
Das-Akash2022 <www.beboosona2115@gmail.com>
2023-02-25 14:45:05 +0530

    addedimage

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navbar } from './components'
+import { Hero, Navbar } from './components'
+
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
 
     </div>
   )
---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import donaTion from '../../src/assets/donaTion.png'
+
+const Hero = () => {
+  return (
+    <div className='w-full bg-white'>
+        <div className='max-w-[1480] m-auto grid grid-cols-2'>
+            <div></div>
+            <img src={donaTion} />
+
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Hero
