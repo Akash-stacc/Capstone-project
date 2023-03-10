46e7c54768337ffeef36d18a4f57fb709f0e53d7
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-03-10 15:37:42 +0530

    changes

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import { Hero, Navbar,Companies, Meals } from './components'
 import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignupPage} />
+        {/* Add your other routes here */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 
 
