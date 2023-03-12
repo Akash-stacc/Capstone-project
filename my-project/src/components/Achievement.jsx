41d16679ac4ac14672d22e904288b4000f946ee7
myuser <dasbapariakash@gmail.com>
2023-03-12 16:31:10 +0530

    completedachievement

--- my-project/src/components/Achievement.jsx
+++ my-project/src/components/Achievement.jsx
@@ -1,16 +1,77 @@
 import React from 'react'
 import achievement from '../../src/assets/Achievement.png'
-
-
+import {SlGraduation} from 'react-icons/sl'
+import {FiVideo} from 'react-icons/fi'
+import {SlPeople} from 'react-icons/sl'
 const Achievement = () => {
   return (
     <div className='w-full bg-white py-24'>
         <div className='md:max-w-[1480px] m-auto grid md:grid-cols-2 md:max-w-[600px] '>
 
 
-            <div className='flex flex-col justify-start gap-4'>
-              <h1 className='md:leading-[72px] py-2 text-3xl font-bold'>Our <span className='text-[#20B486]'>Achievements</span></h1>
-              <p className='py-2 text-lg text-gray-600'>Actions speak louder than words! Give today</p>
+            <div className='flex flex-col justify-center'>
+              <h1 className='md:leading-[72px] text-3xl font-bold'>Our <span className='text-[#20B486]'>Achievements</span></h1>
+              <p className='text-lg text-gray-600'>Actions speak louder than words! Give today</p>
+            
+              <div className='grid grid-cols-2 py-16 '>
+                  <div className='py-6 flex'>
+                      <div className='py-4 bg-[#E9F8F3] rounded-xl'>
+                        <SlGraduation
+                            size={30}
+                            style={{color:'#1A906B'}}
+                        
+                        /> 
+                      </div>
+                      <div className='px-3'>
+                        <h1 className='text-2xl font-semibold'>300</h1>
+                        <p className='text-[#6D737A]'>Graduates volunteer</p>
+                      </div>
+
+                  </div>
+                  <div className='py-6 flex'>
+                      <div className='py-4 bg-[#FFFAF5] rounded-xl'>
+                        <FiVideo
+                            size={30}
+                            style={{color:'#FFC27A'}}
+                        
+                        /> 
+                      </div>
+                      <div className='px-3'>
+                        <h1 className='text-2xl font-semibold'>10,000+</h1>
+                        <p className='text-[#6D737A]'>Video</p>
+                      </div>
+                      
+
+                  </div>
+                  <div className='py-6 flex'>
+                      <div className='py-4 bg-[#FFEEF0] rounded-xl'>
+                        <SlGraduation
+                            size={30}
+                            style={{color:'#ED4459'}}
+                        
+                        /> 
+                      </div>
+                      <div className='px-3'>
+                        <h1 className='text-2xl font-semibold'>20,000+</h1>
+                        <p className='text-[#6D737A]'>Student's volunteer</p>
+                      </div>
+
+                  </div>
+                  <div className='py-6 flex'>
+                      <div className='py-4 bg-[#F0F7FF] rounded-xl'>
+                        <SlPeople
+                            size={30}
+                            style={{color:'#0075FD'}}
+                        
+                        /> 
+                      </div>
+                      <div className='px-3'>
+                        <h1 className='text-2xl font-semibold'>1,00,000+</h1>
+                        <p className='text-[#6D737A]'>Active Donor</p>
+                      </div>
+
+                  </div>
+              </div>
                           </div>
 
               
